dfad7daeb49c27179645bb6eedd92ebbe7c68f60
Rasheduzzaman Sourov <rasheduzzaman.sourov@enosisbd.com>
2017-01-10 20:21:37 +0600

    **Update**

--- Source/ogldev/ogldev/ShaderMaterial.cpp
+++ Source/ogldev/ogldev/ShaderMaterial.cpp
@@ -1,6 +1,8 @@
 #include "ShaderMaterial.h"
 #include "Utility.h"
 
+using namespace std;
+
 ShaderMaterial::ShaderMaterial(std::string vertex_shader_source, std::string fargment_shader_source, std::string geometry_shader_source, std::string tessellation_shader_source) : shader_program_identifier(0)
 {
 	this->vertex_shader_source = vertex_shader_source;
@@ -19,4 +21,14 @@ ShaderMaterial::~ShaderMaterial()
 GLuint ShaderMaterial::getShaderProgram()
 {
 	return shader_program_identifier;
+}
+
+void ShaderMaterial::addUniform1i(const string uniform_name, int value)
+{
+	integer_uniforms.push_back(make_pair(uniform_name, value));
+}
+
+void ShaderMaterial::addUniform1f(const std::string uniform_name, float value)
+{
+	float_uniforms.push_back(make_pair(uniform_name, value));
 }
--- Source/ogldev/ogldev/ShaderMaterial.h
+++ Source/ogldev/ogldev/ShaderMaterial.h
@@ -3,6 +3,7 @@
 #include "RenderMaterial.h"
 #include "glew.h"
 #include <string>
+#include <vector>
 
 class ShaderMaterial : public RenderMaterial
 {
@@ -12,6 +13,9 @@ public:
 
 	GLuint getShaderProgram();
 
+	void addUniform1i(const std::string uniform_name, int value);
+	void addUniform1f(const std::string uniform_name, float value);
+
 protected:
 	std::string vertex_shader_source;
 	std::string fragment_shader_source;
@@ -19,4 +23,7 @@ protected:
 	std::string tessellation_shader_source;
 
 	GLuint shader_program_identifier;
+
+	std::vector<std::pair<std::string, int>>   integer_uniforms;
+	std::vector<std::pair<std::string, float>> float_uniforms;
 };
